257755a8f1f22f139b5eaacb6bdecf95bf78b55e
poonam-kumawat <mailpoonam2002@gmail.com>
2023-07-04 00:49:55 +0530

    added scroller

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import Home from './component/Home/home';
+import Homepage from './component/homepage/homepage';
 
 
 function App() {
   return (
     <div className='overAll'>
       <Home/>
+      <Homepage/>
     </div>
   );
 }
--- src/component/Home/home.js
+++ src/component/Home/home.js
@@ -5,10 +5,10 @@ import './home.css';
 class Home extends Component {
   render() {
     return (
-      <div className='headerWrap'>
+      <div className='headerWrap fixed'>
         <div className='header'>
         <div className='header-content'>
-            <img src="" alt='logo_img'/>
+            <img src="/assets/svg/logo.svg" alt='logo_img'/>
         </div>
         <div className='content-menu'>
             <h1>HOME.</h1>
@@ -16,7 +16,7 @@ class Home extends Component {
             <h1>CONTACT.</h1>
         </div>
         <div className='content-btn'>
-            <button className='text-white font-bold py-2 px-4 btnDownload'>Download</button>
+            <button className='text-white font-bold py-2 px-12 align-middle inline-flex btnDownload'>Login</button>
         </div>
 
         </div>
