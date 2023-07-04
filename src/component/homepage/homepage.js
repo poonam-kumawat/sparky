1a3a4077157815a4036dcb77d71afa704196bcf7
poonam-kumawat <mailpoonam2002@gmail.com>
2023-07-04 23:43:36 +0530

    added image

--- src/component/homepage/homepage.js
+++ src/component/homepage/homepage.js
@@ -6,7 +6,13 @@ class Homepage extends Component {
   render() {
     return (
       <div className='content-home'>
-      <img src='/assets/svg/galaxy.svg' className='bg-cover bg-no-repeat bg-center' alt='img'></img>
+     <div className='container pt-10 text-center'>
+      <h1 className='text-white pt-10 text-center'>Cosmic Conquest</h1>
+      <h1 className='text-white pt-10 text-center'> Interstellar Odyssey</h1>
+      <div className='content-btn pt-8'>
+            <button className='text-white font-bold py-2 px-12  align-middle inline-flex btnDownload'>Download</button>
+        </div>
+      </div>
         
       </div>
     );
