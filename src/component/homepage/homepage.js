2ca6ae8a9f085d840738780c053da814daffcab5
poonam-kumawat <mailpoonam2002@gmail.com>
2023-07-06 00:28:02 +0530

    changed theme

--- src/component/homepage/homepage.js
+++ src/component/homepage/homepage.js
@@ -6,12 +6,19 @@ class Homepage extends Component {
   render() {
     return (
       <div className='content-home'>
-     <div className='container pt-10 text-center'>
-      <h1 className='text-white pt-10 text-center'>Cosmic Conquest</h1>
-      <h1 className='text-white pt-10 text-center'> Interstellar Odyssey</h1>
-      <div className='content-btn pt-8'>
-            <button className='text-white font-bold py-2 px-12  align-middle inline-flex btnDownload'>Download</button>
+           <div className='container mx-auto  grid grid-cols-2'>
+           <div className='leftWrap pt-16'>
+      <h1 className='text-white pt-10 text-left text-6xl'>Cosmic Conquest</h1>
+      <h1 className='text-white pt-10 text-left text-5xl'> Interstellar Odyssey</h1>
+      <div className='content-btn pt-8 text-left'>
+            <button className='text-white text-2xl font-bold py-3 px-12  inline-flex btnDownload'>Download</button>
         </div>
+        </div>
+        <div className='rightWrap'>
+          <img src="/assets/svg/styrda.svg"></img>
+        </div>
+        
+
       </div>
         
       </div>
