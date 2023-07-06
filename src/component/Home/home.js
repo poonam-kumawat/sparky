2c6d7bdf50db70cc1d792e979e99f3793cc4cce7
poonam-kumawat <mailpoonam2002@gmail.com>
2023-07-06 23:55:22 +0530

    create mobile reponsive header

--- src/component/Home/home.js
+++ src/component/Home/home.js
@@ -1,28 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './home.css';
 
- 
-class Home extends Component {
-  render() {
+const Home =()=> {  
+  const [showNavbar, setShowNavbar] = useState(false)
+  
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
     return (
       <div className='headerWrap'>
+
         <div className='header'>
+        <nav className='Nav-bar'>
         <div className='header-content'>
             <img src="/assets/svg/logo.svg" alt='logo_img'/>
         </div>
-        <div className='content-menu'>
-            <h1>HOME.</h1>
-            <h1>ABOUT US.</h1>
-            <h1>CONTACT.</h1>
+        <div className="menu-icon" onClick={handleShowNavbar}>          
+            <img src="/assets/svg/hamburger.svg" alt='hamburger' className='w-20 h-20'/>          
         </div>
-        <div className='content-btn'>
-            <button className='text-white font-bold py-2 px-12 align-middle inline-flex btnDownload'>Login</button>
+        <div className={`nav-menu  ${showNavbar && 'active'}`}>
+            <ul className='content-menu'>
+            <div className="Mobmenu-icon" onClick={handleShowNavbar}>          
+            <img src="/assets/svg/close.svg" alt='close' className='w-10 float-right mt-5'/>          
         </div>
+              <li>
+                <a href='/'>HOME.</a>
+              </li>
+              <li>
+                <a href='/'>ABOUT US.</a>
+              </li>
+              <li>
+                <a href='/'>CONTACT.</a>
+              </li>
+              <li>
+              <div className='content-btn'>
+            <button className='text-white font-bold py-2 px-12 align-middle inline-flex  btnlogin'>Login</button>
+        </div>
+              </li>
+            </ul>
+
+        </div>
+        
+        </nav>
 
         </div>
       </div>
     );
-  }
+  
 }
  
 export default Home;
